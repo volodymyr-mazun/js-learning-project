bf523ff583dec05e46cd86af65fd1a08cdf8c96d
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-04-27 16:19:52 +0300

    logical transformations

---
+++ java scirpt/module 2.js
@@ -0,0 +1,39 @@
+//*ОПЕРАТОР SWITCH
+// function getSubscriptionPrice(type) {
+//   switch (type) {
+//     case "starter":
+//       return 0;
+//       break;
+//     case "professional":
+//       return 20;
+//       break;
+//     case "organization":
+//       return 50;
+//     default:
+//       return "Invalid subscription type!";
+//   }
+// }
+
+//*ЛОГІЧНЕ ""І"
+// function isNumberInRange(start, end, number) {
+//   if (start < number && number < end) {
+//     return true;
+//   }
+//   return false;
+// }
+
+//*ЛОГІЧНЕ "АБО"
+// function checkAccess(subType) {
+//   if (subType === "pro" || subType === "vip") {
+//     return true;
+//   }
+//   return false;
+// }
+
+//*ЛОГІЧНЕ "НІ"
+// function toggleModalVisibility(isVisible) {
+//   if (!isVisible) {
+//     return true;
+//   }
+//   return false;
+// }
