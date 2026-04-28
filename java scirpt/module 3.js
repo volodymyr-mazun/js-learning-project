e6b0a14bd076f44e70897b7145f58c82874fcd3c
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-04-28 22:04:50 +0300

    array iteration

---
+++ java scirpt/module 3.js
@@ -0,0 +1,39 @@
+//* ========== ПОШУК ПІДРЯДКА, ВИЗНАЧЕННЯ ІНДЕКСА ТА СТВОРЕННЯ ПІДМАСИВА ==========
+// function getSlice(array, value) {
+//   const isIndexArray = array.indexOf(value);
+//   if (isIndexArray === -1) {
+//     return [];
+//   } else {
+//     const sliceArray = array.slice(0, isIndexArray + 1);
+//     return sliceArray;
+//   }
+// }
+
+//* ========== СТВОРЕННЯ МАСИВА ВСІХ ПАРНИХ ЧМСЕЛ, МЕТОД PUSH==========
+// function getEvenNumbers(start, end) {
+//   const newArray = [];
+//   for (let i = start; i < end; i++) {
+//     if (i % 2 === 0) {
+//       newArray.push(i);
+//     }
+//   }
+//   return newArray;
+// }
+
+//* ========== ІТЕРАЦІЯ ПО МАСИВУ, СУМА ЕЛЕМЕНТІВ ==========
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (let i = 0; i < order.length; i++) {
+//     total += order[i];
+//   }
+//   return total;
+// }
+
+//* ========== ІТЕРАЦІЯ ПО МАСИВУ, СУМА ЕЛЕМЕНТІВ ==========
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (let num of order) {
+//     total += num;
+//   }
+//   return total;
+// }
