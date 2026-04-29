241a8d661cb3cb2641c7ff1c46ff67131e9e4f64
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-04-29 20:51:37 +0300

    object

---
+++ java scirpt/module 4.js
@@ -0,0 +1,7 @@
+//* ========== ОБЧИСЛЮВАЛЬНІ ВЛАСТИВОСТІ ==========
+// const emailInputName = "email";
+// const passwordInputName = "password";
+// const credentials = {
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam",
+// };
