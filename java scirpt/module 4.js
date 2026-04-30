4dfda35d3ffb3f68a45fc3e76f1cc1d92ebe7719
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-04-30 22:02:16 +0300

    object.keys

--- java scirpt/module 4.js
+++ java scirpt/module 4.js
@@ -5,3 +5,19 @@
 //   [emailInputName]: "henry.carter@aptmail.com",
 //   [passwordInputName]: "jqueryismyjam",
 // };
+
+//* ========== СТВОРЕННЯ МАСИВІВ ЦИКЛОМ FOR...IN ==========
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+
+//* ========== СТВОРЕННЯ МАСИВІВ OBJECT.KEYS(), FOR...OF ==========
+// function countProps(object) {
+//   const arrayValues = [];
+//   const arrayKeys = Object.keys(object);
+//   for (const key of arrayKeys) {
+//     arrayValues.push(object[key]);
+//   }
+//   return arrayValues.length;
+// }
