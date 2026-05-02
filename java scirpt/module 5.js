7dd592659acda01ff3245a866d7480f787f07708
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-05-02 23:50:26 +0300

    method foreach

---
+++ java scirpt/module 5.js
@@ -0,0 +1,19 @@
+//* ========== CALLBACK ФУНКЦІЯ ТА ІНЛАЙН-КОЛБЕК ==========
+// function registerGuest(name, callback) {
+//   console.log(`Registering ${name}!`);
+//   callback(name);
+// }
+// registerGuest("Mango", function greet(name) {
+//   console.log(`Welcome ${name}!`);
+// });
+
+//* ========== МЕТОД FOREACH ==========
+// function filterArray(numbers, value) {
+//   const newArray = [];
+//   numbers.forEach(function (elelement) {
+//     if (elelement > value) {
+//       newArray.push(elelement);
+//     }
+//   });
+//   return newArray;
+// }
