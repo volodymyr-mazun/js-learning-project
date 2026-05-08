ca64bd6fb32f2728902fdf72684253faa5bb1d79
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-05-08 21:31:27 +0300

    prototype

--- java scirpt/module 6.js
+++ java scirpt/module 6.js
@@ -48,3 +48,21 @@
 //     console.log(key);
 //   }
 // }
+
+//* ========== КЛАС, ЕКЗЕМПЛЯР, КОНСТРУКТОР, МЕТОДИ. ==========
+// class User {
+//   constructor(params) {
+//     this.name = params.name;
+//     this.email = params.email;
+//   }
+//   getEmail() {
+//     return this.email;
+//   }
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// const mango = new User({
+//   name: "Mango",
+//   email: "mango@mail.com",
+// });
