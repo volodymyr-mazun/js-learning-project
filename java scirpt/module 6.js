edf9c1a66d52d96effe8278b29b4468026e8dd8b
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-05-09 11:53:52 +0300

    getter and setter

--- java scirpt/module 6.js
+++ java scirpt/module 6.js
@@ -66,3 +66,34 @@
 //   name: "Mango",
 //   email: "mango@mail.com",
 // });
+
+//* ========== ГЕТТЕРИ, СЕТТЕРИ, ПРИВАТНІ ВЛАСТИВОСТІ. ==========
+
+// class Car {
+//   #brand;
+//   #model;
+//   #price;
+//   constructor(params) {
+//     this.#brand = params.brand;
+//     this.#model = params.model;
+//     this.#price = params.price;
+//   }
+//   get brand() {
+//     return this.#brand;
+//   }
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+//   get model() {
+//     return this.#model;
+//   }
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+//   get price() {
+//     return this.#price;
+//   }
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+// }
